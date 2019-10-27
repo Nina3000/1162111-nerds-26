ed4c0ee141fb610f97b34069bcb390634996785e
Nina3000 <31375507+Nina3000@users.noreply.github.com>
2019-10-27 14:49:27 +0300

    Поправила карту

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,3 @@
-
 var link = document.querySelector(".mail-button");
 
 var popup = document.querySelector(".mail");
@@ -13,29 +12,29 @@ link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("mail-show");
   name.focus();
-}
+});
 
-  close.addEventListener("click", function (evt) {
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("mail-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!name.value || !email.value) {
     evt.preventDefault();
-    popup.classList.remove("mail-show");
     popup.classList.remove("modal-error");
-  });
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
+});
 
-  form.addEventListener("submit", function (evt) {
-    if (!name.value || !email.value) {
-      evt.preventDefault();
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("mail-show")) {
+      popup.classList.remove("mail-show");
       popup.classList.remove("modal-error");
-      popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("modal-error");
     }
-  });
-
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (popup.classList.contains("mail-show")) {
-        popup.classList.remove("mail-show");
-        popup.classList.remove("modal-error");
-      }
-    }
-  });
+  }
+});
