9aac3a5960e05d5ea04cb68f73f080d3d377442a
Nina3000 <31375507+Nina3000@users.noreply.github.com>
2019-10-30 22:40:18 +0300

    Ещё

--- js/popup.js
+++ js/popup.js
@@ -1,40 +1,40 @@
 var link = document.querySelector(".mail-button");
 
-var popup = document.querySelector(".mail");
-var close = popup.querySelector(".close");
+var popupEl = document.querySelector(".mail");
+var close = popupEl.querySelector(".close");
 
-var form = popup.querySelector("form");
-var name = popup.querySelector("[name=name]");
-var email = popup.querySelector("[type=email]");
+var formEl = popupEl.querySelector("formEl");
+var nameInput = popupEl.querySelector("[type=text]");
+var emailInput = popupEl.querySelector("[type=email]");
 
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add("mail-show");
-  name.focus();
+  popupEl.classList.add("mail-show");
+  nameInput.focus();
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.remove("mail-show");
-  popup.classList.remove("modal-error");
+  popupEl.classList.remove("mail-show");
+  popupEl.classList.remove("modal-error");
 });
 
-form.addEventListener("submit", function (evt) {
-  if (!name.value || !email.value) {
+formEl.addEventListener("submit", function (evt) {
+  if (!nameInput.value || !emailInput.value) {
     evt.preventDefault();
-    popup.classList.remove("modal-error");
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("modal-error");
+    popupEl.classList.remove("modal-error");
+    popupEl.offsetWidth = popupEl.offsetWidth;
+    popupEl.classList.add("modal-error");
   }
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popup.classList.contains("mail-show")) {
-      popup.classList.remove("mail-show");
-      popup.classList.remove("modal-error");
+    if (popupEl.classList.contains("mail-show")) {
+      popupEl.classList.remove("mail-show");
+      popupEl.classList.remove("modal-error");
     }
   }
 });
